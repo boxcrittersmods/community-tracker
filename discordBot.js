08ae2671a2d84d4a80b09da6435a11f9a8d9c8aa
Tumble <tumblegamer@gmail.com>
2020-09-21 12:07:48 +0100

    lookup function

--- discordBot.js
+++ discordBot.js
@@ -1,41 +1,101 @@
 const Discord = require("discord.js");
 const path = require('path');
 const request = require("request");
+const CritterAPI = require("./critterapi/critterapi.js")
 
 const client = new Discord.Client();
+const apt = new CritterAPI();
+var playerIds = {};
 
 client.on('ready', () => {
-	client.user.setPresence({ game: { name: 'P1', type: "LISTENING", }, status: 'online' });
+	client.user.setPresence({ game: { name: 'the love P1 back game', type: "PLAYING", }, status: 'online' });
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
+function lookNice(data) {
+	var embed = {
+		color: 0x0099ff,fields:[]};
 
+	for (const key in data) {
+		switch (key) {
+			case "nickname":
+				embed.title = data[key];
+			break;
+			case "critterId":
+				data[key] = data[key]||"hamster";
+			default:
+				embed.fields.push({
+					name:key,
+					value:data[key].toString()||"N/A"
+				});
+				break;
+		}
+	}
+	return {embed};
+}
 
 function test(cb) {
 	request({ url: "https://boxcritters.com/data/player/EF99F6E35EDA62C", method: 'GET', headers: { 'Accept': 'application/json', 'Accept-Charset': 'utf-8', 'User-Agent': 'Node-Request' } }, cb)
 }
 
+function lookUp(url,cb) {
+	request({ url, method: 'GET', headers: { 'Accept': 'application/json', 'Accept-Charset': 'utf-8', 'User-Agent': 'Node-Request' } }, cb)
+}
+
+
 
 var commands = {
-	"ping": function (message, args) {
+	"ping":{args:[],description:"test command",call: function (message, args) {
 		message.channel.send("yay");
-	},
-	"echo": function (message, args) {
-		message.channel.send(args.join(" "));
-	},
-	"test":function(message,args) {
-		test(function(err,res,body){message.channel.send(body)})
-	},
-	"invite":function(message,args) {
+	}},
+	"echo": {args:["message"],description:"says what you say",call:function (message, args) {
+		message.channel.send(args.join(" "))
+	}},
+	"invite":{args:[],description:"share the bot",call:function(message,args) {
 		message.channel.send("https://discord.com/oauth2/authorize?client_id=757346990370717837&scope=bot");
-	}
+	}},
+	"help":{args:[],description:"lists help commands",call:function(message,args) {
+		message.channel.send("Commands: ```"+Object.keys(commands).map(c=>"!bc " + c + " " + commands[c].args.map(a=>"["+a+"]").join(" ") + " - " + commands[c].description).join("\n")+"```Want specific help?: https://discord.gg/D2ZpRUW");
+	}},
+	"lookup":{args:["username"],description:"look at people",call:function(message,args){
+		//message.channel.send("is that you " + message.author + " I know thats you. Well this command hasn't been made yet")
+
+		var nickname = args.join(" ");
+		var id = playerIds[nickname]||nickname;
+
+		lookUp("https://boxcritters.com/data/player/"+id,(err,res,body) =>{
+			if(err) {
+				message.channel.send("Invalid playerId or username. Please look up a playerId to add its username to the database.")
+				return;
+			}
+			try {
+				var data = JSON.parse(body);
+			} catch(e) {
+				message.channel.send("Invalid playerId or username. Please look up a playerId to add its username to the database.")
+				return
+			}
+			if(!playerIds[data.nickname]) {
+				playerIds[data.nickname] = id;
+				message.channel.send(data.nickname + " has been saved to the dectionary as " + id);
+			}
+			message.channel.send(lookNice(data));
+		});
+	}},
+	"dictionary":{args:[],description:"i kow things",call:function(message,args) {
+		message.channel.send("```json\n"+JSON.stringify(playerIds)+"```")
+	}}
 }
 
 function parseCommand(message) {
 	var parts = message.content.split(" ")
 	parts.shift();
-	commands[parts.shift()](message, parts)
+	var cmd = parts.shift();
+	if(!commands[cmd]){
+		console.log("Invalic command " + cmd);
+		return; 
+	}
+	commands[cmd].call(message, parts)
 }
 
 client.on('message', message => {
