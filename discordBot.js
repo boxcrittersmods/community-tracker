29ab3cbba61c7ddd954c90adc2f84d3a3024b962
Tumble <tumblegamer@gmail.com>
2020-09-23 17:14:15 +0100

    prefix

--- discordBot.js
+++ discordBot.js
@@ -451,7 +451,7 @@ client.on('message', message => {
 	if (message.author == client.user || message.author.bot) {
 		return;
 	}
-	if (message.content.toLowerCase().startsWith('!test')) {
+	if (message.content.toLowerCase().startsWith('!bc')) {
 		parseCommand(message).then(console.log).catch(console.error);
 	}
 });
