3d3727cd54fa26bcb6506441c2994c6d0b4a7a84
Tumble <tumblegamer@gmail.com>
2020-11-15 17:38:34 +0000

    dbl env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const discordBot = require('./discordBot.js'),
 	setupDBL = require("./dbl"),
 	token = process.env.DISCORD_TOKEN || require('./config/token.js').token,
-	dblToken = process.env.DISCORD_TOKEN || require('./config/token.js').dblToken;
+	dblToken = process.env.DBL_TOKEN || require('./config/token.js').dblToken;
 
-setupDBL(dblToken, discordBot);
+if (void 0 != dblToken) setupDBL(dblToken, discordBot);
 discordBot.login(token);
