53bd889d6fee37fde46231f582ed4d6649d2531f
Tumble <tumblegamer@gmail.com>
2020-12-17 22:26:07 +0000

    added all items

--- watcher.js
+++ watcher.js
@@ -19,10 +19,14 @@ interval = 120e3,
 			query: async () => await lists.rooms.getJson(),
 			equality: (a, b) => a.roomId == b.roomId,
 		}),
-		createWatcher("items", {
+		createWatcher("rooms", {
+			query: async () => await lists.items.getJson(),
+			equality: (a, b) => a.itemId == b.itemId,
+		}),
+		createWatcher("codes", {
 			query: async () => {
 				let codes = await itemCodeList.getJson(),
-					shopItems = (await lists.shops.getJson()).flatMap(shop => shop.collection?shop.collection.map(e => ({ name: e, dateReleased: shop.startDate, code: "Available in the shop" })):[]),
+					shopItems = (await lists.shops.getJson()).flatMap(shop => shop.collection ? shop.collection.map(e => ({ name: e, dateReleased: shop.startDate, code: "Available in the shop" })) : []),
 					roomItems = (await lists.rooms.getJson()).flatMap(room => room.triggers.filter(trigger => trigger.server && trigger.server.grantItem).map(trigger => ({ name: trigger.server.grantItem, code: `Found in ${room.name}` })));
 				return codes.concat(shopItems, roomItems).sort((a, b) => new Date(b.dateReleased) - new Date(a.dateReleased));
 			},
