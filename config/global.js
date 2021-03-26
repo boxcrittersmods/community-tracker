cbcab9b5ba75ddd5d0b84ba15aeb8b2e97a8f953
Tumble <tumblegamer@gmail.com>
2021-03-26 10:22:53 +0000

    bcmc.ga

--- config/global.js
+++ config/global.js
@@ -1,10 +1,10 @@
-const path = require("path")
+const path = require("path");
 
-var bcmcAPI = "https://api.boxcrittersmods.ga";
+var bcmcAPI = "https://api.bcmc.ga";
 
 var iTrackBC = {
-    root: path.join(__dirname,".."),
-    require:(id)=>require(path.join(iTrackBC.root,id)),
+    root: path.join(__dirname, ".."),
+    require: (id) => require(path.join(iTrackBC.root, id)),
     token: {
         discord: process.env.DISCORD_TOKEN || require('./token').token,
         dbl: process.env.DBL_TOKEN || require('./token').dblToken,
@@ -14,22 +14,22 @@ var iTrackBC = {
         password: process.env.DB_PASSWORD || require("./token").dbPassword,
         url: "playerdictionary.mftw9.mongodb.net/lookupBot"
     },
-    prefix: process.env.LOCAL ?"!test":"!bc",
-    wikiPages:require("./wikiPages.json"),
+    prefix: process.env.LOCAL ? "!test" : "!bc",
+    wikiPages: require("./wikiPages.json"),
     bcAPI: {
-        players:"https://base.boxcritters.com/data/players.json?id="
+        players: "https://base.boxcritters.com/data/players.json?id="
     },
-    bcmcAPI:{
-        root:bcmcAPI,
-        items:bcmcAPI+"/manifests/items",
-        rooms:bcmcAPI+"/manifests/rooms",
-        shops:bcmcAPI+"/manifests/shops",
-        files:bcmcAPI+"/textures/BoxCritters.bctp.json",
-        itemCodes:bcmcAPI+"/itemcodes",
-        playerGear:bcmcAPI+"/player/",
-        roomPreview:bcmcAPI+"/room/",
-        staticRoomPreview:bcmcAPI+"/room/static/"
+    bcmcAPI: {
+        root: bcmcAPI,
+        items: bcmcAPI + "/manifests/items",
+        rooms: bcmcAPI + "/manifests/rooms",
+        shops: bcmcAPI + "/manifests/shops",
+        files: bcmcAPI + "/textures/BoxCritters.bctp.json",
+        itemCodes: bcmcAPI + "/itemcodes",
+        playerGear: bcmcAPI + "/player/",
+        roomPreview: bcmcAPI + "/room/",
+        staticRoomPreview: bcmcAPI + "/room/static/"
     }
-}
+};
 
 global.iTrackBC = iTrackBC;
